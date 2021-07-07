8762b0ac6f9136432a3f4ed18c04d5e2a9de4cce
Emile <emilefeltesse@gmail.com>
2021-07-07 16:17:23 +0200

    Modif sur challenges

---
+++ SoloLearn/src/challenges/Test.java
@@ -0,0 +1,15 @@
+package challenges;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int x = (1>0)?0:1;
+		System.out.println(x);
+		x=x++;
+		System.out.println(x);
+		x++;
+		System.out.println(x);
+
+	}
+
+}
